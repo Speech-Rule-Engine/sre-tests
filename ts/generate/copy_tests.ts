f2b8ee98a93c5df3fa25ce19a215abf2a5b76c8d
zorkow <volker.sorge@gmail.com>
2020-12-15 13:24:28 +0000

    Adds direct speech tests for all locales.

---
+++ ts/generate/copy_tests.ts
@@ -0,0 +1,48 @@
+// Copyright 2020 Volker Sorge
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Methods for copying and transformng test files.
+ *
+ * @author volker.sorge@gmail.com (Volker Sorge)
+ */
+
+import {JsonFile, TestPath, TestUtil} from '../base/test_util';
+import {get as factoryget} from '../classes/test_factory';
+import {addMissing} from './fill_tests';
+
+/**
+ * Copies and adapts a single test file from one locale to another.
+ * @param source The expected source file.
+ */
+export function copyTestLocale(source: string, locale: string,
+                               loc1: string, loc2: string) {
+  let tests = factoryget(source);
+  let dst = [TestPath.EXPECTED, locale].concat(
+    source.split('/').slice(1)).join('/');
+  tests.jsonTests.tests = {};
+  tests.jsonTests.locale = locale;
+  replaceInTests(tests.jsonTests, 'name', loc1, loc2);
+  replaceInTests(tests.jsonTests, 'active', loc1, loc2);
+  replaceInTests(tests.jsonTests, 'information', loc1, loc2);
+  TestUtil.saveJson(dst, tests.jsonTests);
+  addMissing(dst);
+}
+
+function replaceInTests(
+  tests: JsonFile, key: string, what: string, by: string) {
+  if (tests[key]) {
+    tests[key] = tests[key].replace(what, by);
+  }
+}
